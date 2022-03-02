ef70817f7b1a20a299f363ed643ae81feab6bc16
davitjnz <djanezashvili@gmail.com>
2022-03-02 22:38:59 +0400

    annotations

---
+++ annotation.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+from typing import Optional, List
+from uuid import UUID
+
+from pydantic import Field, BaseModel
+from uuid import UUID, uuid4
+from beanie import Document
+
+
+class TextForAnnotation(Document):
+    id: UUID = Field(default_factory=uuid4, alias='_id')
+    post_id: Optional[UUID]
+    words: List[str]
+
+    class Collection:
+        name = 'text_for_annotation'
+
+class Annotation(BaseModel):
+    tag_id: int
+    label: str
+    words: List[int]
+    relation: Optional[str]
+    labelGrup: str
+
+
+class Annotations(Document):
+    id: UUID = Field(default_factory=uuid4, alias='_id')
+    text_id: UUID
+    user_mail: str
+    annotations: Optional[List[Annotation]]
+
+    class Collection:
+        name = 'annotations'
