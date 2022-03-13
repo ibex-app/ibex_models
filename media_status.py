bb036352b1f911729e0109244fce6b8a2fa74f23
davitjnz <djanezashvili@gmail.com>
2022-03-13 18:56:55 +0400

    fix typo

--- media_status.py
+++ media_status.py
@@ -4,4 +4,4 @@ class MediaStatus(str, Enum):
     to_be_downloaded = 'to_be_downloaded'
     downloaded = 'downloaded'
     no_download = 'no_download'
-    processoed = 'processoed'
+    processed = 'processed'
