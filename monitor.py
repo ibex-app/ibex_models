d999d90ca314709ebe6477c4ba61c944140466cd
Davit <4066753+davitjnz@users.noreply.github.com>
2022-10-05 18:02:43 +0400

    Update monitor.py
    add monitor statuses

--- monitor.py
+++ monitor.py
@@ -5,6 +5,18 @@ from pydantic import Field
 from uuid import UUID, uuid4
 from datetime import datetime
 
+from enum import Enum
+
+class MonitorStatus(int, Enum):
+    initialized = 0
+    sampling = 1
+    sampled = 2
+    live = 3
+    collecting = 4
+    collected = 5
+    finalized = 6
+
+
 class Monitor(Document):
     id: UUID = Field(default_factory=uuid4, alias='_id')
     title: str
@@ -12,6 +24,7 @@ class Monitor(Document):
     collect_actions: List[UUID]
     date_from: datetime
     date_to: Optional[datetime]
+    status: MonitorStatus = MonitorStatus.initialized
 
     class Collection:
         name = "monitors"
